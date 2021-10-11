044153a0b56b999706f2b5e9170841daa9738267
“hliunian <shaojiangYUE@yeah.net>
2021-10-11 23:23:58 +0800

    Initial commit

---
+++ src/test/java/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * @Project: test_idea_github
+ * @Author:
+ * @Create: 2021-10-11 23:22
+ * @Desc：
+ **/
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
